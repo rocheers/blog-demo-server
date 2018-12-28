60adedccdf09320a9767fe69134c65fd0fd1b09c
Aaron Zhang <aaronz.osu@gmail.com>
2018-12-27 18:57:54 -0800

    Adding Signin Functionality

--- handlers/auth.js
+++ handlers/auth.js
@@ -1,7 +1,48 @@
 const db = require('../models');
 const jwt = require('jsonwebtoken');
 
-exports.signin = function() {};
+exports.signin = async function(req, res, next) {
+    // TODO : try catch block two scopes so that error message might not
+    // be proper for one of them.
+    try {
+        // finding a user
+        let user = await db.User.findOne({
+            email: req.body.email
+        });
+        let { id, username, profileImageUrl } = user;
+
+        // checking if the password matches what was sent to the server
+        let isMatch = await user.comparePassword(req.body.password);
+
+        // if it matches, log in
+        if (isMatch) {
+            let token = jwt.sign(
+                {
+                    id,
+                    username,
+                    profileImageUrl
+                },
+                process.env.SECRETE_KEY
+            );
+            return res.status(200).json({
+                id,
+                username,
+                profileImageUrl,
+                token
+            });
+        } else {
+            return next({
+                status: 400,
+                message: 'Invalid Email/Password'
+            });
+        }
+    } catch (e) {
+        return next({
+            status: 400,
+            message: 'Invalid Email/Password'
+        });
+    }
+};
 
 exports.signup = async function(req, res, next) {
     try {
