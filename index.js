3eb47554db578b4422e64ff23383fd7ca359b5bc
Aaron Zhang <aaronz.osu@gmail.com>
2019-01-17 18:44:02 -0800

    get some variables from env

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const authRouter = require('./routes/auth');
 const messagesRouter = require('./routes/messages');
 const { loginRequired, ensureCorrectUser } = require('./middleware/auth');
 const db = require('./models');
-const PORT = 8081;
+const PORT = process.env.PORT || 8081;
 
 app.use(cors());
 app.use(express.json());
--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 mongoose.set('debug', true);
 mongoose.Promise = Promise;
 mongoose.connect(
-    'mongodb://localhost/warbler',
+    process.env.MONGODB_URL || 'mongodb://localhost/warbler',
     {
         keepAlive: true,
         useNewUrlParser: true
